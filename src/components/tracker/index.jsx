8d959a19ffb4bfc1360b4a7926cc4f82da743b46
Matheus Rodrigues <matheusrf96@gmail.com>
2024-01-17 15:40:25 -0300

    feat (tracker): Load tracker script

--- src/components/tracker/index.jsx
+++ src/components/tracker/index.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react"
+
 const Tracker = (props) => {
+    useEffect(() => {
+        const script = document.createElement('script')
+        script.src = 'http://localhost:8000/cmp' + window.location.search
+        script.async = true
+
+        document.body.appendChild(script)
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <>
-            <script id="cmp" async src={ 'http://localhost:8000/cmp' + window.location.search }></script>
             <input type="hidden" id="eh" name="eh" value={props.apiKey} />
             <input type="hidden" id="tv" name="tv" value={props.page} />
         </>
